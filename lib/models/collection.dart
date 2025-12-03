bebd10623f50e627bb0ad2c3486ee4ed1caf3818
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 14:49:13 +0000

    Add category field to Collection model

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -2,10 +2,12 @@ class Collection {
   final String title;
   final String image;
   final String route;
+  final String category; // Add this field
 
   Collection({
     required this.title,
     required this.image,
     required this.route,
+    required this.category,
   });
 }
