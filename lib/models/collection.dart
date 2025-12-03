3e7d9c005e00e89b958514483e6178514652c56d
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 14:37:09 +0000

    Add Collection model to represent product collections

---
+++ lib/models/collection.dart
@@ -0,0 +1,11 @@
+class Collection {
+  final String title;
+  final String image;
+  final String route;
+
+  Collection({
+    required this.title,
+    required this.image,
+    required this.route,
+  });
+}
