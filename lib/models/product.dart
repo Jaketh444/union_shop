29a570a0d97f89e7a1de34d88f7054bceec4a05b
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 15:12:06 +0000

    Add Product model with necessary fields and constructor

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String title;
+  final String image;
+  final String category;
+  final String description;
+  final double price;
+
+  Product({
+    required this.title,
+    required this.image,
+    required this.category,
+    required this.description,
+    required this.price,
+  });
+}
