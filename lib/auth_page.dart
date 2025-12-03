05c5b15d7222cad8b1e9668ef0abacd949eb9d79
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 12:40:34 +0000

    Refactor AuthPage to use StatefulWidget for dynamic state management

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool showSignUp = false;
 
   @override
   Widget build(BuildContext context) {
