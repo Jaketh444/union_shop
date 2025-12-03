44e8e8f81089dadd97939c0569ea54dddf7786bc
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 16:09:33 +0000

    Refactor Product model to use a list for categories and update product lists accordingly

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,14 +1,14 @@
 class Product {
   final String title;
   final String image;
-  final String category;
+  final List<String> categories; // Change from String to List<String>
   final String description;
   final double price;
 
   Product({
     required this.title,
     required this.image,
-    required this.category,
+    required this.categories,
     required this.description,
     required this.price,
   });
