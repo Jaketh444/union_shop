567afd2a3e9719d0c9f6a8fb06a3e187c7127341
Jaketh444 <up2274964@myport.ac.uk>
2025-12-03 15:40:39 +0000

    Add new apparel and accessory collections to CollectionsPage

--- lib/collections_page.dart
+++ lib/collections_page.dart
@@ -23,6 +23,31 @@ final List<Collection> allCollections = [
       route: '/jackets',
       category: 'Apparel'),
   Collection(
+      title: 'Sweatpants',
+      image: 'assets/sweatpants.png',
+      route: '/sweatpants',
+      category: 'Apparel'),
+  Collection(
+      title: 'Caps',
+      image: 'assets/caps.png',
+      route: '/caps',
+      category: 'Apparel'),
+  Collection(
+      title: 'Socks',
+      image: 'assets/socks.png',
+      route: '/socks',
+      category: 'Apparel'),
+  Collection(
+      title: 'Scarves',
+      image: 'assets/scarves.png',
+      route: '/scarves',
+      category: 'Apparel'),
+  Collection(
+      title: 'Gloves',
+      image: 'assets/gloves.png',
+      route: '/gloves',
+      category: 'Apparel'),
+  Collection(
       title: 'Accessories',
       image: 'assets/accessories.png',
       route: '/accessories',
@@ -42,7 +67,46 @@ final List<Collection> allCollections = [
       image: 'assets/sale.png',
       route: '/sale',
       category: 'Sale'),
-  // Add more collections as needed
+  Collection(
+      title: 'Pins',
+      image: 'assets/pins.png',
+      route: '/pins',
+      category: 'Accessories'),
+  Collection(
+      title: 'Lanyards',
+      image: 'assets/lanyards.png',
+      route: '/lanyards',
+      category: 'Accessories'),
+  Collection(
+      title: 'Water Bottles',
+      image: 'assets/water_bottles.png',
+      route: '/waterbottles',
+      category: 'Drinkware'),
+  Collection(
+      title: 'Tote Bags',
+      image: 'assets/tote_bags.png',
+      route: '/totebags',
+      category: 'Bags'),
+  Collection(
+      title: 'Backpacks',
+      image: 'assets/backpacks.png',
+      route: '/backpacks',
+      category: 'Bags'),
+  Collection(
+      title: 'Mugs',
+      image: 'assets/mugs.png',
+      route: '/mugs',
+      category: 'Drinkware'),
+  Collection(
+      title: 'Notebooks',
+      image: 'assets/notebooks.png',
+      route: '/notebooks',
+      category: 'Accessories'),
+  Collection(
+      title: 'Gift Cards',
+      image: 'assets/giftcards.png',
+      route: '/giftcards',
+      category: 'General'),
 ];
 
 class CollectionsPage extends StatefulWidget {
